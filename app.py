4c061682e0b755c63524930606798cb5d3bd4a68
Roshan095 <gangaramroshanminaal@gmail.com>
2025-10-31 13:53:51 +0530

    traning

---
+++ app.py
@@ -0,0 +1,16 @@
+
+print("Hello, World!")  
+a=23
+b=42
+print(a + b)
+print("hello world")
+#primitive
+a=30 #"integer"
+b=56 #"string"
+c=True #"boolean"
+#non primitive
+e = (23,35,18)
+f = [23,54,89]
+print(type (f))
+h = {id: '23'}
+print(type(h))
